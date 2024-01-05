e8838219c01b26ef47b13b6e3d28c98ccbedf501
wsmolarski <wmsmolarski@gmail.com>
2024-01-05 01:50:27 +0100

    generating fixed

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const passwordBox = document.getElementById("password");
-const lenght = 16;
+document.addEventListener("DOMContentLoaded", function() {
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "!@#$%^&*()_+~|}{[]></-=";
+    const passwordBox = document.getElementById("password");
+    const length = 16;
 
-const allChars = upperCase + lowerCase + number + symbol;
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const number = "0123456789";
+    const symbol = "!@#$%^&*()_+~|}{[]></-=";
 
-function createPassword(){
-    let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
-    password += symbol[Math.floor(Math.random() * symbol.length)];
+    const allChars = upperCase + lowerCase + number + symbol;
 
-    while(lenght > password.length){
-        password += allChars[Math.floor(Math.random() * allChars.length)];
+    function createPassword(){
+        let password = "";
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += number[Math.floor(Math.random() * number.length)];
+        password += symbol[Math.floor(Math.random() * symbol.length)];
+
+        while(length > password.length){
+            password += allChars[Math.floor(Math.random() * allChars.length)];
+        }
+        passwordBox.value = password;
     }
-    passwordBox.value = password;
-}
+       
+    const generateButton = document.getElementById("generateBtn");
+    generateButton.addEventListener("click", createPassword);
+});
