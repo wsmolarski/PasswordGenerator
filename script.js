c39d4786a086d8b5b4790d4f62bafa7dc40e38e0
wsmolarski <wmsmolarski@gmail.com>
2024-01-05 15:58:14 +0100

    finished

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const allChars = upperCase + lowerCase + number + symbol;
 
-    function createPassword(){
+    function createPassword() {
         let password = "";
         password += upperCase[Math.floor(Math.random() * upperCase.length)];
         password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
         password += number[Math.floor(Math.random() * number.length)];
         password += symbol[Math.floor(Math.random() * symbol.length)];
 
-        while(length > password.length){
+        while(length > password.length) {
             password += allChars[Math.floor(Math.random() * allChars.length)];
         }
         passwordBox.value = password;
@@ -25,4 +25,15 @@ document.addEventListener("DOMContentLoaded", function() {
        
     const generateButton = document.getElementById("generateBtn");
     generateButton.addEventListener("click", createPassword);
+
+    const copyButton = document.getElementById("copyBtn");
+    copyButton.addEventListener("click", function() {
+        if (passwordBox.value) {
+            navigator.clipboard.writeText(passwordBox.value).then(() => {
+                alert("Password copied to clipboard!");
+            })
+        }else {
+            alert("Generate a password first!");
+        }
+    });
 });
